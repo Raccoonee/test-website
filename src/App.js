af986be972d2fa83578b282c681ae1341c6d1928
Devin Dennis <ddevin099@gmail.com>
2022-01-15 21:36:33 -0600

    clean up unused code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 import DataTable from "./DataTable";
 
 function App() {
-  const [resourceType, setResourceType] = useState("posts");
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then((response) => response.json())
-      .then((json) => setItems(json));
-  }, [resourceType]);
 
   return (
     <>
-      {/* <div>
-        <button className="button1" onClick={() => setResourceType("posts")}>
-          posts
-        </button>
-        <button className="button2" onClick={() => setResourceType("users")}>
-          users
-        </button>
-        <button className="button3" onClick={() => setResourceType("comments")}>
-          commments
-        </button>
-      </div>
-      <h1>{resourceType}</h1> */}
       <h1>WEED DATA 🌿</h1>
       <DataTable />
       <h2>Created by Devin Dennis</h2>
